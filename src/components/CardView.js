6258c879817f592d322cba46e40e4727f9d31fd9
Victoria Kirst <vrk@glitch.com>
2020-08-11 09:25:33 -0400

    fix linter errors

--- src/components/CardView.js
+++ src/components/CardView.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState } from "react";
 import OpenCard from "./OpenCard";
 import Envelope from "./Envelope";
 
